e4ad040031fbc8ece1f6fe1b8e59fcc5332356e9
Emiri Sato <emirisato.1003@gmail.com>
2025-04-28 06:42:19 -0700

    add server.js

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
+import { HiH1 } from "react-icons/hi2";
 
-export default function Vans(){
-    return(
-        <h1>Vans page goes here🚐</h1>
-    )
+export default function Vans() {
+    const [vans, setVans] = useState([]);
+    useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data));
+    }, []);
+    
+    return (
+        <h1>vans page goes here</h1>
+    );
 }
