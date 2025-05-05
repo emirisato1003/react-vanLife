b15ac09f1a1b5e5bad87b4b64ee572e66302518f
Emiri Sato <emirisato.1003@gmail.com>
2025-05-05 09:12:00 -0700

    get van detail page from url

--- src/pages/vanDetail.jsx
+++ src/pages/vanDetail.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
+    const [vanDetail, setVanDetail] = useState(null);
     const params = useParams();
-    console.log(params);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(`/api/vans/${params.id}`);
+                if (!res.status) {
+                    throw new Error(res.status);
+                }
+                const { vans } = await res.json();
+                setVanDetail(vans);
+            } catch (err) {
+                console.log(err.message);
+            }
+        })();
+    }, [params.id]); // ensure it fetches data whenever the ID changes
     return (
-        <h1>Van detail here</h1>
+        <div className="van-detail-container">
+            {vanDetail ? (
+                <div className="van-detail">
+                    <img src={vanDetail.imageUrl} />
+                    <i className={`van-type ${vanDetail.type} selected`}>{vanDetail.type}</i>
+                    <h2>{vanDetail.name}</h2>
+                    <p className="van-price"><span>${vanDetail.price}</span>/day</p>
+                    <p>{vanDetail.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
     );
 }
