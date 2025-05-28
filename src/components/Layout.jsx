ea2c083b18a3224938a368777e408868ae31cfca
Emiri Sato <emirisato.1003@gmail.com>
2025-05-28 10:33:49 -0700

    reorganize folders and files.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
-import Vans from '../pages/Vans';
-import VanDetail from '../pages/vanDetail';
+import Vans from '../pages/Vans/Vans';
+import VanDetail from '../pages/Vans/vanDetail';
 import Header from './Header';
 
 export default function Layout() {
