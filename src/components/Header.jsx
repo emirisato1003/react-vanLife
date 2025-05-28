4223ca783b8f55f9872121ccda02c630e6bce50e
Emiri Sato <emirisato.1003@gmail.com>
2025-05-27 18:05:29 -0700

    add header styling module css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export default function Header() {
     return (
         <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
+            <Link className={styles.logo} to="/">#VanLife</Link>
             <nav>
                 <Link to="/about">About</Link>
                 <Link to="/vans">Vans</Link>
