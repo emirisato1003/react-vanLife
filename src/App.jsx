5bc35c80f3bd2d432fd95e6a9ea5a5d3badd13b7
Emiri Sato <emirisato.1003@gmail.com>
2025-05-28 10:45:11 -0700

    add Host link to the navbar. create host folders and nested components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import About from './pages/About';
 import Vans from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/vanDetail';
 import Layout from './components/Layout';
+import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
 import "./server";
 import './App.css';
 
@@ -21,6 +24,9 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/vans" element={<Vans />} />
             <Route path="/vans/:id" element={<VanDetail />} />
+            <Route path='/host' element={<Dashboard />} />
+            <Route path='/host/income' element={<Income />} />
+            <Route path='/host/reviews' element={<Reviews />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Host/Income.jsx
@@ -0,0 +1,4 @@
+
+export default function Income() {
+    return <h1>Income page</h1>;
+}
