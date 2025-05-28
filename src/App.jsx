98ab90dcdc47451fdf42c7e464f63e893d2345d6
Emiri Sato <emirisato.1003@gmail.com>
2025-05-27 17:13:19 -0700

    set up Layout component and render outlet and Header component in Layout comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Vans from './pages/Vans';
-// import VanDetail from './pages/vanDetail';
+import Home from './pages/Home';
+import About from './pages/About';
+import Vans from './pages/Vans';
+import VanDetail from './pages/vanDetail';
 import Layout from './components/Layout';
 import "./server";
 import './App.css';
@@ -15,12 +15,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header>{children}</Header> */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       <footer>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Vans from '../pages/Vans';
 import VanDetail from '../pages/vanDetail';
 import Header from './Header';
 
-export default function Layout({ children }) {
+export default function Layout() {
     return (
-        pass
+        <>
+            <Header />
+            <Outlet />
+        </>
     );
 }
