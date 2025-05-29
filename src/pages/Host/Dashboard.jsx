d53932e40d30806eceb40839e4fa5d3899368627
Emiri Sato <emirisato.1003@gmail.com>
2025-05-29 10:28:19 -0700

    set navigation bar in hostLayout component, and styling it.

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,8 +1,7 @@
-import { Outlet } from "react-router-dom";
+// import { Link, Outlet } from "react-router-dom";
 
 export default function Dashboard() {
     return <>
-        <Outlet />
         <h1>Dashboard page goes here</h1>
     </>;
 }
--- src/pages/Host/HostLayout.jsx
+++ src/pages/Host/HostLayout.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import styles from '../Host/HostLayout.module.css'
 export default function HostLayout() {
-    return;
-    <>
-        <Outlet />
-    </>;
+    return (
+        <>
+            <ul className={styles.hostNav}>
+                <li className={styles.hostNavList}><Link to="/host">Dashboard</Link></li>
+                <li className={styles.hostNavList}><Link to="/host/income">Income</Link></li>
+                <li className={styles.hostNavList}><Link to="/host/vans">Vans</Link></li>
+                <li className={styles.hostNavList}><Link to="/host/reviews">Reviews</Link></li>
+            </ul>
+            < Outlet />
+        </>
+    );
 }
