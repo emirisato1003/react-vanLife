7836a992b9a0f10ba6d7e26cc5d4c8501e52648b
Emiri Sato <emirisato.1003@gmail.com>
2025-06-05 10:18:54 -0700

    #

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 export default function HostVanInfo() {
+    const context = useOutletContext()
+    console.log(context)
     return (
         <div>
             <h2>Host Van info goes here</h2>
