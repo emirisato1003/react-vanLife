c68dc662ac157799974d0d361322dced2574ce55
Emiri Sato <emirisato.1003@gmail.com>
2025-07-05 10:20:50 -0700

    complete vanilla html in dashboard.jsx

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,32 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from '../components/HostLayout.module.css';
+import { getVans } from "../api";
+import { useState, useEffect } from "react";
 export default function HostLayout() {
+    const [hostVans, setHostVans] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const navStyle = {
         fontWeight: 'bold',
         textDecoration: 'underline',
         color: '#161616'
     };
+
+    const HostVansFetch = async () => {
+        setLoading(true);
+        try {
+            const data = await getVans('/api/host/vans');
+            setHostVans(data);
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    };
+    useEffect(() => {
+        HostVansFetch();
+    }, []);
     return (
         <div className="host-wrapper">
             <nav className={styles.hostNav}>
@@ -15,7 +35,7 @@ export default function HostLayout() {
                 <NavLink style={({ isActive }) => isActive ? navStyle : null} to="vans">Vans</NavLink>
                 <NavLink style={({ isActive }) => isActive ? navStyle : null} to="reviews">Reviews</NavLink>
             </nav>
-            < Outlet />
+            < Outlet context={{hostVans, loading}} />
         </div>
     );
 }
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,7 +1,53 @@
 // import { Link, Outlet } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs";
+import { Link, useOutletContext } from "react-router-dom";
+import styles from './Dashboard.module.css';
 
 export default function Dashboard() {
-    return <>
-        <h1>Dashboard page goes here</h1>
-    </>;
+    const { hostVans, loading } = useOutletContext();
+    function renderVanElements(vans) {
+        const hostVansEls = vans.map((van) => (
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+                <Link to={`vans/${van.id}`}>View</Link>
+            </div>
+        ));
+
+        return (
+            <div className="host-vans-list">
+                <section>{hostVansEls}</section>
+            </div>
+        );
+    }
+    return (
+        <>
+            <section className={styles.hostDashboard}>
+                <div className="info">
+                    <h1>Welcome!</h1>
+                    <p>Income last <span>30 days</span></p>
+                    <h2>$2,260</h2>
+                </div>
+                <Link to='income'>Details</Link>
+            </section>
+            <section className="reviewScore">
+                <h2>Review score </h2>
+                <BsStarFill />
+                <p><span>5.0</span>/5</p>
+                <Link to='reviews'>Details</Link>
+            </section>
+            <section className="listedVans">
+                <div className="top">
+                    <h2>Your listed vans</h2>
+                    <Link to='vans'>View all</Link>
+                </div>
+                {
+                    loading && !hostVans ? <h1>Loading...</h1> : <>{renderVanElements(hostVans)}</>
+                }
+            </section>
+        </>
+    );
 }
